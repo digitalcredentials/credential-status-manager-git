dc0b07cd5470dccdcf5611f8258adc8ea391b0de
Kayode Ezike <kezike13@gmail.com>
2023-04-17 00:47:32 -0400

    replace "require" with "import" in post-install.js

--- post-install.js
+++ post-install.js
@@ -1,8 +1,7 @@
-const fs = require('fs');
+import fs from 'fs';
 const wd = process.cwd();
 const packageJsonFileName = 'package.json';
 const packageJsonFilePath = `${wd}/${packageJsonFileName}`;
-const json = require(packageJsonFilePath);
+import json from packageJsonFilePath;
 json.type = 'module';
 fs.writeFileSync(packageJsonFilePath, JSON.stringify(json, null, 2))
-
